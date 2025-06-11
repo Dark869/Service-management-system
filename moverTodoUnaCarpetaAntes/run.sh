832c7e2785caecffd4776ab9b4262037d2b6b577
CarlosAlbertoTamarizMorales <zS21015980@estudiantes.uv.mx>
2025-06-10 22:26:22 -0600

    Plantillas de monitoreo, registro de servidor y servicio

--- moverTodoUnaCarpetaAntes/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sleep 10
-
-python -u manage.py makemigrations
-python -u manage.py migrate
-
-gunicorn --bind :8000 services_management_system.wsgi:application --reload
--- moverTodoUnaCarpetaAntes/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while read -r linea; do    
-    export "$linea"
-done < <(ccdecrypt -c .env.cpt)
-
-docker-compose up -d
-
